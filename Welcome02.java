5bd80e7a9210e134097fc930d583185d886be96c
Chandini <79863466+MoonRock404@users.noreply.github.com>
2024-02-21 20:06:59 -0800

    Create Welcome02.java

---
+++ Welcome02.java
@@ -0,0 +1,39 @@
+import core.data.*;
+
+public class Welcome02_Object {
+   public static void main(String[] args) {
+      String id1 = "KATL";
+      DataSource ds1 = DataSource.connect("http://weather.gov/xml/current_obs/" + id1 + ".xml"); 
+      ds1.setCacheTimeout(15 * 60);  
+      ds1.load();
+      //ds1.printUsageString();
+
+      Observation ob1 = ds1.fetch("Observation", "weather", "temp_f", "wind_degrees");
+      System.out.println(id1 + ": " + ob1);
+
+      String id2 = "KSAV";
+      DataSource ds2 = DataSource.connect("http://weather.gov/xml/current_obs/" + id2 + ".xml"); 
+      ds2.setCacheTimeout(15 * 60);  
+      ds2.load();
+
+      Observation ob2 = ds2.fetch("Observation", "weather", "temp_f", "wind_degrees");
+      System.out.println(id2 + ": " + ob2);
+
+     String id3 = "NSTU";
+     DataSource ds3 = DataSource.connect("http://weather.gov/xml/current_obs/" + id2 + ".xml"); 
+     ds3.setCacheTimeout(15 * 60);  
+     ds3.load();
+
+     Observation ob3 = ds3.fetch("Observation", "weather", "temp_f", "wind_degrees");
+     System.out.println(id3 + ": " + ob3);
+
+     // determines the colder of the three observations and prints the result
+      if (ob1.colderThan(ob2) && ob1.colderThan(ob3)) {
+         System.out.println("Colder at " + id1);
+      } else if (ob2.colderThan(ob1) && ob2.colderThan(ob3)) {
+         System.out.println("Colder at " + id2);
+      } else {
+         System.out.println("Colder at " + id3);
+      }
+   }
+}
